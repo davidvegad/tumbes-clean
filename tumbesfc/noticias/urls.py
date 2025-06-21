cf5fd83ab8d910e700e34564ba8539909c311b32
davidvegacorus <david.vega@corusconsulting.com>
2025-06-21 15:22:11 -0500

    4. Version estable Demo Adrian

--- tumbesfc/noticias/urls.py
+++ tumbesfc/noticias/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.noticias_list, name='noticias_list'),
+    path('<int:pk>/', views.noticia_detalle, name='noticia_detalle'),
+
 ]
--- tumbesfc/noticias/views.py
+++ tumbesfc/noticias/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Noticia
 
 def noticias_list(request):
     noticias = Noticia.objects.order_by('-fecha')[:8]
     return render(request, 'noticias/noticias.html', {'noticias': noticias})
+
+def noticia_detalle(request, pk):
+    noticia = get_object_or_404(Noticia, pk=pk)
+    return render(request, 'noticias/noticia_detalle.html', {'noticia': noticia})
