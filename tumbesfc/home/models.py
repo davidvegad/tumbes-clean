7a26665c7a116d21923d44920790436b0db84258
davidvegacorus <david.vega@corusconsulting.com>
2025-06-24 13:00:52 -0500

    Agregar S3 hero

--- tumbesfc/home/models.py
+++ tumbesfc/home/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from storages.backends.s3boto3 import S3Boto3Storage # <-- 1. Importar
+
+s3_storage = S3Boto3Storage()
 
 class HeroSlide(models.Model):
-    imagen = models.ImageField(upload_to='hero/')
+    imagen = models.ImageField(upload_to='hero/', storage=s3_storage)
     titulo = models.CharField(max_length=100, blank=True)
     subtitulo = models.CharField(max_length=200, blank=True)
     boton_texto = models.CharField(max_length=50, blank=True)
